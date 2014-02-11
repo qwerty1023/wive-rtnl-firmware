06c4a9bb337f83a3aec94f2ad229f30806a6ee88
sandra <sandra@sandra-VirtualBox.(none)>
2014-02-11 09:48:45 +0200

    kernel: conntrack: increase default table size to 8192 for 8k foe tbl config

--- linux-2.6.21.x/net/netfilter/nf_conntrack_core.c
+++ linux-2.6.21.x/net/netfilter/nf_conntrack_core.c
@@ -1633,16 +1633,16 @@ int __init nf_conntrack_init(void)
 	    This is must be set depended at foe table size. And may be changed by /proc */
 #if defined(CONFIG_RAETH_MEMORY_OPTIMIZATION) || defined(CONFIG_RT2860V2_AP_MEMORY_OPTIMIZATION) || \
     defined(CONFIG_RA_HW_NAT_TBL_1K) || defined(CONFIG_RA_HW_NAT_TBL_2K)
-        nf_conntrack_htable_size = 1024;
+        nf_conntrack_htable_size = 2048;
 #else
 #if defined(CONFIG_RA_HW_NAT_TBL_4K)
-        nf_conntrack_htable_size = 2048;
-#elif defined(CONFIG_RA_HW_NAT_TBL_8K)
         nf_conntrack_htable_size = 4096;
-#elif defined(CONFIG_RA_HW_NAT_TBL_16K)
+#elif defined(CONFIG_RA_HW_NAT_TBL_8K)
         nf_conntrack_htable_size = 8192;
+#elif defined(CONFIG_RA_HW_NAT_TBL_16K)
+        nf_conntrack_htable_size = 16384;
 #else
-        nf_conntrack_htable_size = 2048;
+        nf_conntrack_htable_size = 4096;
 #endif
 #endif
         nf_conntrack_max = nf_conntrack_htable_size * 2;
