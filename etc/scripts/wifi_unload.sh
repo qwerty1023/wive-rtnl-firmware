f7d765f88dffb9ebb37160e1d2fd3d7420677444
sandra <sandra@sandra-VirtualBox.(none)>
2014-08-27 23:10:20 +0300

    sync

--- etc/scripts/wifi_unload.sh
+++ etc/scripts/wifi_unload.sh
@@ -9,7 +9,7 @@
 . /etc/scripts/global.sh
 
 stop_serv="minidlna transmission shaper crontab pppoe-relay ddns wscd dhcpd lld2d radvd zebra udpxy upnp \
-	    hotplug igmp_proxy ntp samba dnsserver snmpd xupnp minidlna vpnhelper syslog inetd"
+	    hotplug igmp_proxy ntp samba dnsserver snmpd xupnp vpnhelper syslog inetd"
 
 kill_apps="minidlnad transmission-daemon smbd nmbd pppd xl2tpd udhcpd udhcpc crond lld2d igmpproxy inetd syslogd klogd \
 	    ntpclient ntpd zebra ripd inadyn ftpd scp miniupnpd \
